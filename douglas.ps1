4e0995c02c9b021ffbf9f0e429deef20bd6dc329
龙在天 <73683679+LztCode@users.noreply.github.com>
2024-05-20 16:26:10 +0800

    添加中文
    添加所有用户（包括隐藏账户）的检查

--- douglas.ps1
+++ douglas.ps1
@@ -35,138 +35,142 @@ $blue = (gi env:\Computername).Value
 Write-Host "Collecting data for $blue ($ip) | $(Get-Date -Format dd/MM/yyyy-H:mm:ss)"
 
 $data = {
-"==== GENERAL INFORMATION ===="
+"==== 一般信息 ===="
 #Get-ComputerInfo | Format-List -Property CsDNSHostName, CsDomain, OsName, OsVersion, OsBuildNumber, OsArchitecture, OsUptime, OsLocalDateTime, TimeZone, OsSerialNumber, OsMuiLanguages, OsHotFixes, WindowsRegisteredOrganization, WindowsRegisteredOwner, WindowsSystemRoot, OsPagingFiles, CsManufacturer, CsModel, CsName, CsProcessors, CsNetworkAdapters, BiosBIOSVersion, BiosSeralNumber, BiosFirmwareType, CsDomainRole, OsStatus, OsSuites, LogonServer, DeviceGuardSmartStatus, DeviceGuardRequiredSecurityProperties, DeviceGuardAvailableSecurityProperties, DeviceGuardSecurityServicesConfigured, DeviceGuardSecurityServicesRunning, DeviceGuardCodeIntegrityPolicyEnforcementStatus, DeviceGuardUserModeCodeIntegrityPolicyEnforcementStatus
 systeminfo
 "------------------------------------------------------------------------------------------------------------------------------------
 "
 
-"--- Group policy settings ---"
+"--- 组策略设置 ---"
 gpresult.exe -z
 "----------------------------------------
 "
 
-"--- Encryption information ---"
+"--- 加密信息 ---"
 manage-bde.exe -status
 "----------------------------------------
 "
-"==== ACCOUNT AND GROUP INFORMATION ===="
-"--- LOCAL USER ---"
+"==== 账户和组信息 ===="
+"--- 所有用户（包括隐藏账户） ---"
+Get-WmiObject -Class Win32_UserAccount
+"----------------------------------------
+"
+"--- 本地用户 ---"
 Get-LocalUser
 "----------------------------------------
 "
-"--- Enabled Local User ---"
+"--- 启用的本地用户 ---"
 Get-LocalUser | ? Enabled -eq "True"
 "----------------------------------------
 "
-"--- Local Group ---"
+"--- 本地组 ---"
 Get-LocalGroup
 "----------------------------------------
 "
-"--- Local Group Administrator ---"
+"--- 本地组管理员 ---"
 Get-LocalGroup Administrators
 "----------------------------------------
 "
-"--- Account Settings  ---"
+"--- 账户设置 ---"
 net accounts
 "----------------------------------------
 "
 
 
-"==== OS BUILD AND HOTFIXES ===="
-"--- Hotfixes ---"
+"==== 操作系统版本和修补程序 ===="
+"--- 修补程序 ---"
 Get-HotFix
 "----------------------------------------
 "
-"--- Operating System ---"
+"--- 操作系统 ---"
 Get-CimInstance Win32_OperatingSystem | Select-Object Caption, Version, Servicepackmajorversion, BuildNumber, CSName, LastBootUpTime
 Get-ItemProperty "HKLM:\SOFTWARE\MICROSOFT\windows NT\CurrentVersion" | Select-Object ReleaseId
 "----------------------------------------
 "
 
 
-"==== HARDWARE QUERIES ===="
-"--- Bios Information ---"
+"==== 硬件查询 ===="
+"--- BIOS信息 ---"
 gcim -ClassName Win32_BIOS | fl Manufacturer, Name, SerialNumber, Version;
 "-----------------------------------------------------------------------------
 "
-"--- Processor Information ---"
+"--- 处理器信息 ---"
 gcim -ClassName Win32_Processor | fl caption, Name, SocketDesignation;
 "-----------------------------------------------------------------------------
 "
-"--- Information Manufacturer, SystemFamily,Model,SystemType ---"
+"--- 制造商、系统系列、型号、系统类型信息 ---"
 gcim -ClassName Win32_ComputerSystem | fl Manufacturer, Systemfamily, Model, SystemType
 "----------------------------------------
 "
-"---Information About Logical Disk Drives---"
+"--- 逻辑磁盘驱动器信息 ---"
 gcim  -ClassName Win32_LogicalDisk
 gcim  -ClassName Win32_LogicalDisk |Select -Property DeviceID, DriveType, @{L='FreeSpaceGB';E={"{0:N2}" -f ($_.FreeSpace /1GB)}}, @{L="Capacity";E={"{0:N2}" -f ($_.Size/1GB)}} | fl
 "----------------------------------------
 "
 
-"==== FIREWALL INFORMATION ===="
-"--- Configuration of the Windows Firewall: ---"
+"==== 防火墙信息 ===="
+"--- Windows防火墙配置 ---"
 netsh advfirewall show currentprofile
 "----------------------------------------
 "
-"--- Firewall Profile ---"
+"--- 防火墙配置文件 ---"
 Get-NetFirewallProfile
 "----------------------------------------
 "
-"--- Firewall Settings ---"
+"--- 防火墙设置 ---"
 Get-NetFirewallSetting
 "----------------------------------------
 "
-"--- Inactive Firewall ---"
+"--- 未启用的防火墙 ---"
 Get-NetFirewallRule | Where-Object { $_.Enabled -ne $true }
 "----------------------------------------
 "
-"==== NETWORK INFORMATION ===="
-"--- Active Network Interfaces ---"
+"==== 网络信息 ===="
+"--- 活动网络接口 ---"
 Get-NetAdapter | ? status -eq "up" |  Get-NetIPAddress | Select IPAddress,InterfaceIndex, InterfaceAlias, AddressFamily,PrefixOrigin |Sort InterfaceAlias | Format-Table -Wrap
 "----------------------------------------
 "
-"--- Active TCP connections Remote IP ---"
+"--- 活动的TCP连接远程IP ---"
 (Get-NetTCPConnection).remoteaddress | Sort-Object -Unique
 "----------------------------------------
 "
-"--- List UDP endpoints ---"
+"--- 列出UDP端点 ---"
 Get-NetUDPEndpoint | select local*,creationtime, remote* | ft -autosize
 "----------------------------------------
 "
-"--- Network Ipv6 addresses  ---"
+"--- 网络IPv6地址 ---"
 Get-NetIPAddress -AddressFamily IPv6  | ft Interfacealias, IPv6Address
 "----------------------------------------
 "
-"--- Shows TCP connections on the Internet ---"
+"--- 显示互联网上的TCP连接 ---"
 Get-NetTCPConnection -AppliedSetting Internet | select-object -property remoteaddress, remoteport, creationtime | Sort-Object -Property creationtime | format-table -autosize
 "----------------------------------------
 "
 
-"==== CHECK HOST FILE ===="
-"--- DNS cache ---"
+"==== 检查主机文件 ===="
+"--- DNS缓存 ---"
 Get-DnsClientCache
 "----------------------------------------
 "
-"----DNS cache Success----"
+"---- DNS缓存成功 ----"
 Get-DnsClientCache -Status 'Success' | Select Name, Data
 "----------------------------------------
 "
 
 
-"--- Hosts File & Attributes ---"
+"--- 主机文件及属性 ---"
 gc "C:\Windows\System32\Drivers\etc\hosts"
 gci "C:\Windows\System32\Drivers\etc\hosts" | fl *Time* 
 "----------------------------------------
 "
 
-"====SHARED FOLDERS===="
+"==== 共享文件夹 ===="
 net use
 "----------------------------------------
 "
 
-"==== PROCESS INFORMATION ===="
-"--- Process Connections ---"
+"==== 进程信息 ===="
+"--- 进程连接 ---"
 $nets = netstat -bano|select-string 'TCP|UDP'; 
 foreach ($n in $nets)    
 {
@@ -177,167 +181,167 @@ $n -replace "$($nar[-1])","$($ppath) $($pname)";
 }
 "----------------------------------------
 "
-"--- Running processes ---"
+"--- 正在运行的进程 ---"
 tasklist /v /fo table /fi "STATUS ne Unknown"
 "----------------------------------------
 "
-"--- Process AppData ---"
+"--- 进程AppData ---"
 get-process | ?{$_.Path -like '*appdata*'}
 "----------------------------------------
 "
-"--- Process AppData Detailed ---"
+"--- 进程AppData详细信息 ---"
 get-process | select name, path, starttime, ID | ?{$_.Path -like '*appdata*'} | fl
 "----------------------------------------
 "
-"--- Process List ---"
+"--- 进程列表 ---"
 Get-Process -IncludeUserName | Format-Table -Property Name, Id, Path, UserName, Company, Handles, StartTime, HasExited -Wrap
 "----------------------------------------
 "
-"--- Top 7 CPU Usage   ---"
+"--- 前7个CPU使用率 ---"
 Get-Process | Sort-Object CPU -Descending | Select-Object -First 7 | Format-Table Name, CPU, WorkingSet -AutoSize
 "----------------------------------------
 "
-"--- Top 7 MEMORY Usage   ---"
+"--- 前7个内存使用率 ---"
 Get-Process | Sort-Object WorkingSet -Descending | Select-Object -First 7 | Format-Table Name, WorkingSet -AutoSize
 "----------------------------------------
 "
-"--- Process Commandline ---"
+"--- 进程命令行 ---"
 Get-WmiObject Win32_Process | Select-Object Name,  ProcessId, CommandLine | Sort Name | Format-Table -Wrap
 "----------------------------------------
 "
 
 
-"==== PERSISTENCE ===="
-"--- Commands on Startup ---"
+"==== 持久性 ===="
+"--- 启动命令 ---"
 Get-CimInstance -Class Win32_StartupCommand | Format-Table -Property Name, Command, User, Location -Wrap
 "----------------------------------------
 "
-"--- Scheduled Tasks ---"
+"--- 计划任务 ---"
 (Get-ScheduledTask).Where({$_.State -ne "Disabled"}) | Sort TaskPath | Format-Table -Wrap
 "----------------------------------------
 "
-"--- Scheduled Tasks (WIFI) ---"
+"--- 计划任务（WIFI） ---"
 Get-ScheduledTask -Taskname "wifi*" | fl *
 "----------------------------------------
 "
 
 
-"======== SERVICE QUERIES ======="
-"--- Basic Services Information ---"
+"======== 服务查询 ======="
+"--- 基本服务信息 ---"
 Get-Service | Select-Object Name, DisplayName, Status, StartType
 "----------------------------------------
 "
-"--- Detailed Service Information ---"
+"--- 详细的服务信息 ---"
 Get-WmiObject win32_service | Select-Object Name, PathName, StartName, StartMode, State, ProcessId | Sort PathName| Format-Table -Wrap
 #Get-CimInstance -Class Win32_Service -Filter "Caption LIKE '%'" | Select-Object Name, PathName, ProcessId, StartMode, State | Format-Table
 "----------------------------------------
 "
-"--- Automatic Service Information ---"
+"--- 自动服务信息 ---"
 Get-Service | Select-Object Name,DisplayName, Status,StartType | where StartType -eq "Automatic"
 "----------------------------------------
 "
-"--- Running Service Information ---"
+"--- 正在运行的服务信息 ---"
 Get-Service | Select-Object Name,DisplayName, Status,StartType | where Status -eq "Running"
 "----------------------------------------
 "
-"--- EventLog Service ---"
+"--- 事件日志服务 ---"
 get-service -name "eventlog" | fl *
 "----------------------------------------
 "
 
 
-"======== INSTLLATION Of SOFTWARE ======="
+"======== 软件安装 ======="
 Get-CimInstance -ClassName win32_product | Select-Object Name,Version, Vendor, InstallDate, InstallSource, PackageName, LocalPackage |  Format-Table -Wrap
 "----------------------------------------
 "
 
 
-"==== USER ACTIVITY ===="
-"--- Recently used USB devices ---"
+"==== 用户活动 ===="
+"--- 最近使用的USB设备 ---"
 Get-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Enum\USBSTOR\*\* | Select FriendlyName
 "----------------------------------------
 "
-"--- Recently modified files ---"
+"--- 最近修改的文件 ---"
 $RecentFiles = Get-ChildItem -Path $env:USERPROFILE -Recurse -File
 $RecentFiles | Sort-Object LastWriteTime -Descending | Select-Object -First 50 FullName, LastWriteTime
 "----------------------------------------
 "
 
-"--- PowerShell history ---"
+"--- PowerShell历史记录 ---"
 Get-History
 "----------------------------------------
 "
-"--- Kerberos sessions ---"
+"--- Kerberos会话 ---"
 klist sessions
 "----------------------------------------
 "
 
 
 "==== SMB QUERIES ===="
-"--- Smb sessions ---"
+"--- SMB会话 ---"
 Get-SmbSession
 "----------------------------------------
 "
-"--- SMB share ---"
+"--- SMB查询 ---"
 Get-SmbShare; Get-SmbShare | Select-Object Dialect, ServerName, ShareName | Sort-Object Dialect
 "----------------------------------------
 "
-"--- SMB connection ---"
+"--- SMB连接 ---"
 Get-SmbConnection
 "----------------------------------------
 "
 
 
-"==== REMOTING QUERIES ===="
-"--- RDP sessions ---"
+"==== 远程查询 ===="
+"--- RDP会话 ---"
 qwinsta /counter
 "----------------------------------------
 "
-"--- RDP status Enabled-Disabled ---"
+"--- RDP状态启用-禁用 ---"
 if ((Get-ItemProperty "hklm:\System\CurrentControlSet\Control\Terminal Server").fDenyTSConnections -eq 0){write-host "RDP Enabled" } else { echo "RDP Disabled" }
 "----------------------------------------
 "
-"--- PowerShell sessions ---"
+"--- PowerShell会话 ---"
 Get-PSSession
 "----------------------------------------
 "
-"--- PowerShell sessions Configurations ---"
+"--- PowerShell会话配置 ---"
 Get-PSSessionConfiguration | fl Name, PSVersion, Permission
 "----------------------------------------
 "
 
 
-"==== REGISTRY ANALYSIS ===="
-"--- List IWindows registry keys ---"
+"==== 注册表分析 ===="
+"--- 列出Windows注册表项 ---"
 (Gci -Path Registry::).name
 "----------------------------------------
 "
-"--- List HKCU registry keys ---"
+"--- 列出HKCU注册表项 ---"
 Get-ChildItem -Path HKCU:\ | Select-Object -ExpandProperty Name
 "----------------------------------------
 "
-"--- HKCU Properties of items in the Run registry key ---"
+"--- Run注册表项中的HKCU属性 ---"
 Get-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run\"
 "----------------------------------------
 "
-"--- HKLM Properties of items in the Run registry key ---"
+"--- Run注册表项中的HKLM属性 ---"
 Get-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run\" | Get-ItemProperty
 
 
-"==== LOG QUERIES ===="
-"--- Event Log List ---"
+"==== 日志查询 ===="
+"--- 事件日志列表 ---"
 Get-Eventlog -List 
 "----------------------------------------
 "
-"--- Last 20 Application Log ---"
+"--- 最近20条应用程序日志 ---"
 Get-Eventlog Application -Newest 20
 "----------------------------------------
 "
-"--- Last 20 System Log ---"
+"--- 最近20条系统日志 ---"
 Get-Eventlog system -Newest 20
 "----------------------------------------
 "
-"--- Last 20 Security Log ---"
+"--- 最近20条安全日志 ---"
 Get-Eventlog security -Newest 20
 "----------------------------------------
 "
@@ -346,18 +350,18 @@ Get-Eventlog security -Newest 20
 
 if ($a -eq $true)
 {
-"==== ADVANCED INVESTIGATION ===="
-"--- Total Process Instances ---"
+"==== 高级调查 ===="
+"--- 总进程实例 ---"
 Get-Process | Group-Object ProcessName | Select Count, Name | Sort Count -Descending
 "----------------------------------------
 "
 
-"--- Prefetch files ---"
+"--- 预取文件 ---"
 gci C:\Windows\Prefetch\ | Sort Name | Format-Table Name,CreationTime,LastWriteTime,LastAccessTime
 "----------------------------------------
 "
 
-"--- DLL List ---"
+"--- DLL列表 ---"
 gps | Format-List ProcessName, @{l="Modules";e={$_.Modules|Out-String}}
 "----------------------------------------
 "
@@ -367,27 +371,27 @@ Get-WmiObject -Class __FilterToConsumerBinding -Namespace root\subscription | FT
 "----------------------------------------
 "
 
-"--- WMI Filters ---"
+"--- WMI筛选器 ---"
 Get-WmiObject -Class __EventFilter -Namespace root\subscription | FT Name, Query, PSComputerName -wrap
 "----------------------------------------
 "
 
-"--- WMI Consumers ---"
+"--- WMI消费者 ---"
 Get-WmiObject -Class __EventConsumer -Namespace root\subscription | FT Name,ScriptingEngine,ScriptText -wrap
 "-------------------------------------------------------------------
 "
 
-"--- Windows Defender Exclusions ---"
+"--- Windows Defender 排除项 ---"
 Get-ChildItem 'HKLM:\SOFTWARE\Microsoft\Windows Defender\Exclusions'
 "--------------------------------------------------------------------
 "
-"--- List of .exe Files Modified In The Last 3 Days ---"
+"--- 最近 3 天修改的 .exe 文件列表 ---"
 $limit = (Get-Date).AddDays(-3); Get-ChildItem -Path C:\ -Recurse -Include *.exe | Where-Object { $_.LastWriteTime -ge $limit } | ForEach-Object { Write-Host "$($_.Extension) $($_.Name) $($_.LastWriteTime)" }
 "--------------------------------------------------------------------
 "
 
 
-"--- Named Pipes List ---"
+"--- 命名管道列表 ---"
 Get-ChildItem -Path '\\.\pipe\' |  Sort Length | Format-Table FullName, Length, IsReadOnly, Exists, CreationTime, LastAccessTime
 "-------------------------------------------------------------------
 "
